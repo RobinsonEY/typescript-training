1ac9e24e181484a0f00def1bcd207c60ced92fef
Robinson Narvaez <Robinson.Narvaez@pe.ey.com>
2023-05-17 15:12:51 -0500

    add changes to awaited and record

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { MyClassDecorator } from "./02_decorators";
+import { fillNetworkConfigs } from "./09_records";
 
 //02_Decorators
 let myClassDecorator:MyClassDecorator = new MyClassDecorator('tipo');
 
 console.log("myClassDecorator",myClassDecorator.methodDecorators())
+
+fillNetworkConfigs()
